fdc4e153eaf973a1e136a60a17d7b05955f04bf9
Rodrigo <nakataecomm@gmail.com>
2024-07-15 16:48:15 -0300

    melhoria em estrutura condicional de eventos de tocaSom

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
-    console.log(tecla);
     const instrumento = tecla.classList[1];
     
     const idAudio = `#som_${instrumento}`;
@@ -18,8 +17,10 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     }
 
     // quando a tecla for pressionada
-    tecla.onkeydown = function(){
-        tecla.classList.add('ativa');
+    tecla.onkeydown = function(event){
+        if (event.code === 'Enter' || event.code === 'Space') {
+            tecla.classList.add('ativa');
+        }
     }
     tecla.onkeyup = function(){
         tecla.classList.remove('ativa')
