4f3d1816071a1639557b368325867e854ad0a14a
Rodrigo <nakataecomm@gmail.com>
2024-07-15 15:48:12 -0300

    aprimorando a logica

--- main.js
+++ main.js
@@ -1,28 +1,23 @@
 
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
-function tocaSomClap() {
-    document.querySelector('#som_tecla_clap').play();
-}
-function tocaSomTim() {
-    document.querySelector('#som_tecla_tim').play();
-}
-function tocaSomPuff() {
-    document.querySelector('#som_tecla_puff').play();
-}
-function tocaSomSplash() {
-    document.querySelector('#som_tecla_splash').play();
-}
-function tocaSomToim() {
-    document.querySelector('#som_tecla_toim').play();
-}
-function tocaSomPsh() {
-    document.querySelector('#som_tecla_psh').play();
-}
-function tocaSomTic() {
-    document.querySelector('#som_tecla_tic').play();
-}
-function tocaSomTom() {
-    document.querySelector('#som_tecla_tom').play();
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    
+    const idAudio = `#som_${instrumento}`;
+    console.log(idAudio);
+
+    tecla.onclick = function(){
+        tocaSom(idAudio)
+    }
+    contador = contador + 1;
 }
